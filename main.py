a2793d190887ade865237f088d894b9396646534
TranquillouMAN <beney7189@gmail.com>
2023-04-12 15:35:11 +0200

    bruh

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import pygame
 
-pygame.init()
-pygame.display.set_caption("masterclass")
-win=pygame.display.set_mode((800,800))
-run = True
-while run:
-    pygame.time.delay(100)
+def __init__(self):
+    pygame.init()
+    pygame.display.set_caption("masterclass")
+    win=pygame.display.set_mode((1920,1080))
+    run = True
+    x= 40
+    y= 40
+    width = 50
+    height = 50
+    vel = 15
+    self.img = pygame.image.load("img/dingus.jpg").convert_alpha()
+    self.dingus = self.img.get_rect()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-pygame.quit()
+    def render(self,x,y, win):
+        
+        while run:
+            pygame.time.delay(100)
+            win.bLit(self.img, self.dingus)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE] or keys[pygame.KMOD_ALT] and keys[pygame.K_F4]:
+                run = False
+            if keys[pygame.K_LEFT] and x > 5:
+                x-=vel
+            if keys[pygame.K_RIGHT] and x < 800:
+                x+=vel
+            if keys[pygame.K_UP] and y > 5:
+                y -= vel
+            if keys[pygame.K_DOWN] and y < 800:
+                y += vel
+            win.fill((0,0,0))
+            pygame.display.update()
+        pygame.quit()
