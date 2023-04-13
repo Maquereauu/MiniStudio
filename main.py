0cef453ff603dd36d79ceba432843f61f57b7d8d
TranquillouMAN <beney7189@gmail.com>
2023-04-13 14:09:48 +0200

    bruh

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
+from curses import getmouse
 import pygame
 
-def __init__(self):
-    pygame.init()
-    pygame.display.set_caption("masterclass")
-    win=pygame.display.set_mode((1920,1080))
-    run = True
-    x= 40
-    y= 40
-    width = 50
-    height = 50
-    vel = 15
-    self.img = pygame.image.load("img/dingus.jpg").convert_alpha()
-    self.dingus = self.img.get_rect()
-
-    def render(self,x,y, win):
-        
-        while run:
-            pygame.time.delay(100)
-            win.bLit(self.img, self.dingus)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_ESCAPE] or keys[pygame.KMOD_ALT] and keys[pygame.K_F4]:
-                run = False
-            if keys[pygame.K_LEFT] and x > 5:
-                x-=vel
-            if keys[pygame.K_RIGHT] and x < 800:
-                x+=vel
-            if keys[pygame.K_UP] and y > 5:
-                y -= vel
-            if keys[pygame.K_DOWN] and y < 800:
-                y += vel
-            win.fill((0,0,0))
-            pygame.display.update()
-        pygame.quit()
+pygame.init()
+pygame.display.set_caption("masterclass")
+win=pygame.display.set_mode((1920,1080))
+run = True
+x= 0
+y= 0
+width = 50
+height = 50
+vel = 15
+CLOCK = pygame.time.Clock()
+img = pygame.image.load("img/menu.png").convert_alpha()
+img = pygame.transform.scale(img , (1920 , 1080))
+while run:
+    dingus = img.get_rect(topleft = (x,y))
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        run = False
+    win.fill((0,0,0))
+    win.blit(img, dingus)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        run = False
+    CLOCK.tick(165)
+    pygame.display.update()
+pygame.quit()
