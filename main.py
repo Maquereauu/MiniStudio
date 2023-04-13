37063777c9f06e42747eecea8aaffbb5b395f98c
TranquillouMAN <beney7189@gmail.com>
2023-04-13 18:26:30 +0200

    bruh

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ win=pygame.display.set_mode((1920,1080))
 run = True
 x= 40
 y= 40
+c1 = 0 
+c2 = 0
 width = 50
 height = 50
 vel = 15
@@ -16,6 +18,23 @@ img = pygame.image.load("img/menu.png").convert_alpha()
 img = pygame.transform.scale(img , (1920 , 1080))
 bruh =pygame.image.load("img/dingus.jpg").convert_alpha()
 bruh = pygame.transform.scale(bruh , (50 , 50))
+class Rect(pygame.sprite.Sprite):
+    def __init__(self,type,x,y):
+        super().__init__()
+        self.type = type
+        self.c1 = c1
+        self.c2 = c2
+        self.canShoot=True
+
+    def update(self):
+        self.rect.x -= 5
+        if self.type == 2:
+            if self.rect.x < 2000 and self.canShoot:
+                self.canShoot = False
+        if self.rect.x+5 < 0-self.rect.width:
+            self.kill()
+
+rect1 = Rect(2,1500,700)
 while run:
     map = img.get_rect(topleft = (0,0))
     dingus = bruh.get_rect(topleft = (x,y))
